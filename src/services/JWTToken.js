7dea9e4aa3f6e8fc66991a836ddf1f5127c14785
Rafael <>
2022-02-18 07:57:30 -0300

    Update on token issuer validation

--- src/services/JWTToken.js
+++ src/services/JWTToken.js
@@ -3,11 +3,14 @@ import jwt from 'jsonwebtoken';
 import {promisify} from 'util';
 import authConfig from '../config/auth.js';
 
+const algorithm = process.env.JWT_ALGORITHM || 'HS256';
+
 class JWTToken{
     generateToken = async(payload) => {
         try{
             const token = await jwt.sign(payload, authConfig.secret,{
                 expiresIn: authConfig.expiresIn,
+                algorithm: algorithm,
                 issuer: process.env.JWT_ISSUER
             });
             return token;
@@ -19,7 +22,7 @@ class JWTToken{
     validateToken = async(token) => {
         try{
             return await promisify(jwt.verify)(token, authConfig.secret, {
-                algorithms: [process.env.JWT_ALGORITHMS || 'HS256'],
+                algorithms: [algorithm],
                 issuer: process.env.JWT_ISSUER
             });
         }catch(error){
